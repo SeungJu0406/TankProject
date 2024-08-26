52f5344ca586e05c660b7ef5af0df59280375829
SeungJu0406 <nss8164@gmail.com>
2024-08-26 20:41:16 +0900

    feat : 탱크 프리팹 추가, 움직임 구현

---
+++ Assets/Script/TankMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+
+
+    private void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3 (x, 0, z);
+        if (x != 0)
+        {
+            transform.Translate(Vector3.right * x * moveSpeed * Time.deltaTime, Space.World);
+        }
+        if( z!= 0)
+        {
+            transform.Translate(Vector3.forward * z * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        Quaternion rotateDir = Quaternion.LookRotation(moveDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotateDir, rotateSpeed*Time.deltaTime);
+    }
+}
