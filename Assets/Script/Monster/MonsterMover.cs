99b93196d6c148cb5b9f9a1af58f3e29f439cac3
SeungJu0406 <nss8164@gmail.com>
2024-08-28 01:36:34 +0900

    feat : 몬스터 움직임 구현

---
+++ Assets/Script/Monster/MonsterMover.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMover : MonoBehaviour
+{
+    [HideInInspector] Transform target;
+
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] float rotateSpeed;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        transform.LookAt(transform.position);
+    } 
+}
