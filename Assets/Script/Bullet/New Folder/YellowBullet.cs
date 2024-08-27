9d2771c8b66db44a95231e35e43cfbf0d5624c8a
SeungJu0406 <nss8164@gmail.com>
2024-08-27 20:32:08 +0900

    feat: 노란포탄 최대시간 추가
    노란포탄은 직선으로 날아가기때문에 영원히 날아갈 가능성이 존재함

--- Assets/Script/Bullet/New Folder/YellowBullet.cs
+++ Assets/Script/Bullet/New Folder/YellowBullet.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class YellowBullet : Bullet
 {
-
+    [SerializeField] PooledObject pooledObject;
+    [SerializeField] float maxTime;
+    float curTime;
+    private void OnEnable()
+    {
+        curTime = 0;
+    }
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+        if (curTime > maxTime) 
+        {
+            pooledObject.parentPool.ReturnPool(pooledObject);
+        }
+    }
 }
