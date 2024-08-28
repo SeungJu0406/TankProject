94cf4040a8551ed886454815ae72330c227e3367
SeungJu0406 <nss8164@gmail.com>
2024-08-28 23:57:41 +0900

    feat : 움직임 및 총 격발 구현

--- Assets/Script/FPS/FPSController.cs
+++ Assets/Script/FPS/FPSController.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
+    [SerializeField] Transform muzzlePoint;
+
+    [Header("Player Statue")]
+    [SerializeField] float moveSpeed;
+
     [SerializeField] float rotateSpeed;
+    
+    [Header("Gun Status")]
+    [SerializeField] float range;
+
+    [SerializeField] int damage;
+
+    int layerMask;
 
     private void Awake()
-    {      
+    {
+        layerMask = 1 << LayerMask.NameToLayer("Monster");
         rotateSpeed *= 10;
         transform.eulerAngles = Vector3.zero;
     }
     private void Start()
     {
-        Camera.main.transform.position = transform.position;
-        Camera.main.transform.rotation = transform.rotation;
-        Camera.main.transform.parent = transform;
+        Camera.main.transform.position = muzzlePoint.position;
+        Camera.main.transform.rotation = muzzlePoint.rotation;
+        Camera.main.transform.parent = muzzlePoint;
     }
     private void Update()
     {
+        Move();
         Rotate();
+        Fire();
+    }
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3 (x, 0, z);
+        moveDir.Normalize();
+
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
+
+
     }
 
     void Rotate()
@@ -36,6 +63,15 @@ public class FPSController : MonoBehaviour
 
     void Fire()
     {
-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(muzzlePoint.position, muzzlePoint.forward, out RaycastHit hit, range, layerMask))
+            {
+                IHit monster = hit.transform.GetComponent<IHit>();
+                monster.Hit(damage);
+            }
+        }
     }
+
+ 
 }
